db662c89e864986c24a203cce2caac05a9e7e0d5
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-17 21:53:28 +0200

    create accuracy model comparision

--- custom_plots.py
+++ custom_plots.py
@@ -11,12 +11,10 @@ class CustomPlot():
         f.set_figwidth(15)
 
         # Set x-axis range
-        x_axis_range = range(1, len(history['accuracy']) + 1)
+        epoch_range = range(1,len(history['accuracy']) + 1)
 
         # Set plot overall title
         f.suptitle(f"{model_name} Performance", weight='bold')
-
-        epoch_range = range(1,len(history['accuracy']) + 1)
 
         # Plot Accuracy
         ax[0].plot(epoch_range, history['accuracy'])
@@ -24,7 +22,7 @@ class CustomPlot():
         ax[0].legend(['Training accuracy', 'Validation Accuracy'])
         ax[0].set_ylabel('Accuracy', weight='bold')
         ax[0].set_xlabel('Epoch', weight='bold')
-        ax[0].set_xticks(x_axis_range)
+        ax[0].set_xticks(epoch_range)
         ax[0].set_facecolor((0.9, 0.9, 0.9))
         ax[0].grid()
 
@@ -34,8 +32,65 @@ class CustomPlot():
         ax[1].legend(['Training loss', 'Validation loss'])
         ax[1].set_ylabel('Loss', weight='bold')
         ax[1].set_xlabel('Epoch', weight='bold')
-        ax[1].set_xticks(x_axis_range)
+        ax[1].set_xticks(epoch_range)
         ax[1].set_facecolor((0.9, 0.9, 0.9))
-        ax[1].grid()
+        ax[1].grid(True)
 
         plt.savefig(f'{model_name}.png')
+
+    # Function for comparing model perfomance, models have 'accuracy' metric
+    def compare_model_performance_accuracy(history_list, model_name_list=["[Model 1]", "[Model 2]"]):
+
+        f, axs = plt.subplots(2,2)
+
+        # Set figure size
+        f.set_figheight(10)
+        f.set_figwidth(15)
+
+        # Set plot overall title
+        title = ""
+        for i in range(len(model_name_list)):
+            if title == "":
+                title = model_name_list[i]
+            else:
+                title += " vs. " + model_name_list[i]
+        title += " Performance"
+        f.suptitle(title, weight='bold', y=.93, size=20)
+
+        # Set range of epochs for model training
+        epoch_range = range(1,len(history_list[0]['accuracy']) + 1)
+
+        # Create lists of metrics, partial and full names
+        list_of_metrics = ['accuracy', 'val_accuracy', 'loss', 'val_loss']
+        list_of_metrics_fn = ['Accuracy', 
+                                'Validation Accuracy', 
+                                'Loss', 
+                                'Validation Loss']
+
+        # Iterate through subplots
+        for i in range(len(list_of_metrics)):
+            for j in range(len(model_name_list)):
+                # Plot the trainingdata
+                axs.flat[i].plot(epoch_range, history_list[j][list_of_metrics[i]])
+
+                # Set commmon plot styling and labels
+                axs.flat[i].legend(model_name_list)
+                axs.flat[i].set_xticks(epoch_range)
+                axs.flat[i].set_ylabel(list_of_metrics_fn[i], weight='bold')
+                axs.flat[i].set_xlabel('Epoch', weight='bold')
+                axs.flat[i].set_facecolor((0.9, 0.9, 0.9))
+                axs.flat[i].grid(True)
+
+
+        # Set filename
+        filename = ""
+        for i in range(len(model_name_list)):
+            if filename == "":
+                filename = "comp_" + model_name_list[i]
+            else:
+                filename += "_" + model_name_list[i]
+        filename += " .png"
+
+        # Save the file image
+        plt.savefig(filename)
+
--- test.py
+++ test.py
@@ -3,14 +3,24 @@ import numpy as np
 from custom_plots import CustomPlot
 
 # Trouble Shooting
-history = {
+history_1 = {
     'loss': list(np.random.rand(10)),
     'accuracy': list(np.random.rand(10)),
     'val_loss': list(np.random.rand(10)),
     'val_accuracy': list(np.random.rand(10))
 }
 
-CustomPlot.plot_model_performance_accuracy(
-    history=history, 
-    model_name="Troubleshooting"
+history_2 = {
+    'loss': list(np.random.rand(10)),
+    'accuracy': list(np.random.rand(10)),
+    'val_loss': list(np.random.rand(10)),
+    'val_accuracy': list(np.random.rand(10))
+}
+
+list_of_histories = [history_1, history_2]
+models = ["His1", "His2"]
+
+CustomPlot.compare_model_performance_accuracy(
+    history_list=list_of_histories, 
+    model_name_list=models
 )
